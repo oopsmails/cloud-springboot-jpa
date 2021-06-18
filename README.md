623d8a1c0b831e1b088f14681d8d952769f4b352
oopsmails <oopsmails@gmail.com>
2021-06-17 22:49:39 -0400

    20210617: mock repo

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ https://github.com/basahota/springboot-azureSQL
 
 https://www.youtube.com/watch?v=puVMCRvdE9A&list=RDCMUCORuRdpN2QTCKnsuEaeK-kQ&index=2
 
+## Branches
+
+https://github.com/oopsmails/cloud-springboot-jpa/tree/
+
+develop: (not done ... see https://github.com/oopsmails/springboot-app) from spring-boot-mock-backend, pure SpringBoot app with mock Repository.
+
+azure-sqldb: to be deployed on Azure as App Service with SQL Database
+
+
+
+
---
+++ src/main/java/com/oopsmails/cloud/springbootapp/employee/controller/EmployeeController.java
@@ -0,0 +1,51 @@
+package com.oopsmails.cloud.springbootapp.employee.controller;
+
+import com.oopsmails.cloud.springbootapp.employee.model.Employee;
+import com.oopsmails.cloud.springbootapp.employee.repository.EmployeeRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee-api")
+@Slf4j
+public class EmployeeController {
+
+    @Autowired
+    EmployeeRepository repository;
+
+    @GetMapping("")
+// @PreAuthorize("#oauth2.hasScope('read')")
+    public List<Employee> findAll() {
+        return repository.findAll();
+    }
+
+    @PostMapping("")
+// @PreAuthorize("#oauth2.hasScope('write') and #oauth2.hasScope('read')")
+    public Employee add(@RequestBody Employee employee) {
+        return repository.add(employee);
+    }
+
+    @GetMapping("/{id}")
+    public Employee findById(@PathVariable("id") Long id) {
+        return repository.findById(id);
+    }
+
+    @GetMapping("/department/{departmentId}")
+    public List<Employee> findByDepartment(@PathVariable("departmentId") Long departmentId) {
+        return repository.findByDepartment(departmentId);
+    }
+
+    @GetMapping("/organization/{organizationId}")
+    public List<Employee> findByOrganization(@PathVariable("organizationId") Long organizationId) {
+        return repository.findByOrganization(organizationId);
+    }
+}
+
